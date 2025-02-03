2b39b58b1febb3434626b193cbd4952fcde81005
kingmartinlutar <challengegaming98@gmail.com>
2025-02-03 09:17:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = os.getenv("API_ID")
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMINS = [int(admin) for admin in os.getenv("ADMINS").split(",")]
+API_ID = os.getenv("25444644")
+API_HASH = os.getenv("bff60a5f566d9a54c175b0001ba9615b")
+BOT_TOKEN = os.getenv("8056263089:AAHMI88zp5GyHzeaQS82BKNzGFzEI_5DAPs")
+ADMINS = [int(admin) for admin in os.getenv("ADMINS").split("5969730414,")]
